45f1d8d7f427d54d1cbb38fcdfdb5ad450c132b0
jagtar <singhjagtar0290@gmail.com>
2022-04-12 12:34:14 +0530

    CHanges

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpenses/NewExpense";
 function App() {
+const expenses = [
+  {id:"e1",title:"Car Insurance",amount:94.12,date:new Date(2020,7,14)},
+  {id:"e2",title:"Car Insurance",amount:94.12,date:new Date(2020,7,14)},
+  {id:"e3",title:"Car Insurance",amount:94.12,date:new Date(2020,7,14)},
+  {id:"e4",title:"Car Insurance",amount:94.12,date:new Date(2020,7,14)},
+]
+
+const addExpenseHandler = expense => {
+  console.log("In App.js")
+  console.log(expense)
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
 }
